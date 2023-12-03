b007de65014b3a7c415d77f5dcb8b57778693e47
tyronemab <jhonatancastanoibarra@gmail.com>
2023-12-03 09:24:42 -0500

    Reading component

---
+++ server/src/main/java/TextReader.java
@@ -0,0 +1,24 @@
+public class TextReader {
+    
+    String ruta = lectura.next();
+    ArrayList<String> lines = new ArrayList<String>();
+    public void readTxt(){
+
+        try (FileReader fr = new FileReader(ruta)) {
+            BufferedReader br = new BufferedReader(fr);
+            // Lectura del archivo
+
+            
+            String linea;
+            while((linea=br.readLine())!=null)
+                   System.out.println(linea);
+                   lines.add(linea);    
+             }
+             catch(Exception e){
+                e.printStackTrace();
+             }
+
+    }
+   
+
+}
